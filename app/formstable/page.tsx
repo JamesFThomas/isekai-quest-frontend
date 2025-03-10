2c283066f16932b36c807ec21ab080de4af223f9
James F. Thomas <jthomas@rsimail.com>
2025-03-10 11:42:40 -0500

    Data being fetched in FormsTableView then successfully passed && rendered in FormsTable

--- app/formstable/page.tsx
+++ app/formstable/page.tsx
@@ -1,70 +1,47 @@
 'use client';
 
-import {
-  useEffect,
-  // useMemo, // TODO - Uncomment this line to use useMemo
-  useState,
-} from 'react';
+import { useEffect, useState } from 'react';
 
 import FormsTable from '@/components/FormsTable';
-import { mockForms } from '@/data/mockForms';
 
 export type Form = {
-  Id: number;
-  FirstName: string;
-  LastName: string;
-  Message: string;
-  Completed: boolean;
+  id: number;
+  firstName: string;
+  lastName: string;
+  message: string;
+  completed: boolean;
 };
 
 export const dynamic = 'force-dynamic';
 
 export default function FormsTableView() {
-  const [forms, setForms] = useState<Form[]>([]);
-
-  // TODO - Uncomment the fetchData function below to fetch data from the API
-  // const fetchData = useMemo(
-  //   () => async () => {
-  //     fetch('https://localhost:5001/Forms', {
-  //       method: 'GET',
-  //       headers: {
-  //         Accept: 'text/plain',
-  //         'Content-Type': 'application/json',
-  //         'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-  //         'Access-Control-Allow-Credentials': 'true', // Required for cookies, authorization headers with HTTPS
-  //       },
-  //     })
-  //       .then((response) => response.json())
-  //       .then((data) => setForms(data))
-  //       .catch((error) => console.error('Fetch Error', error));
-  //   },
-  //   [],
-  // );
+  const [forms, setForms] = useState<Form[] | undefined>(undefined);
 
   useEffect(() => {
-    // setForms(mockForms);
-    const fetchData = async () => {
-      fetch('https://localhost:5001/Forms', {
-        method: 'GET',
-        headers: {
-          Accept: 'text/plain',
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-          'Access-Control-Allow-Credentials': 'true', // Required for cookies, authorization headers with HTTPS
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => setForms(data))
-        .catch((error) => console.error('Fetch Error', error));
-    };
-    fetchData();
-  }, []);
+    if (!forms) {
+      const fetchData = async () => {
+        const fetchedData = await fetch('https://localhost:5001/Forms', {
+          method: 'GET',
+          headers: {
+            Accept: 'text/plain',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+            'Access-Control-Allow-Credentials': 'true', // Required for cookies, authorization headers with HTTPS
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => data)
+          .catch((error) => console.error('Fetch Error', error));
 
-  // console.log(forms);
+        setForms(fetchedData);
+      };
+      fetchData();
+    }
+  }, [forms]);
 
-  if (forms.length === 0) {
+  if (forms === undefined) {
     return <h1>Loading...</h1>;
   }
 
-  return <FormsTable forms={mockForms} />; // TODO - Change mockForms to forms once you figure out why fetched forms won't render
+  return forms && <FormsTable formsData={forms} />;
 }
--- components/FormsTable.tsx
+++ components/FormsTable.tsx
@@ -1,42 +1,56 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import { Form } from '../app/formstable/page';
 
 type FormsTableProps = {
-  forms: Form[];
+  formsData: Form[];
 };
 
-const FormsTable = ({ forms }: FormsTableProps) => {
-  console.log(forms);
+const FormsTable = ({ formsData }: FormsTableProps) => {
+  const [forms, setForms] = useState<Form[] | undefined>(undefined);
+
+  useEffect(() => {
+    if (!forms) {
+      setForms(formsData);
+    }
+  }, [forms, formsData]);
+
+  console.log(formsData);
+
+  if (!forms) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Forms Table</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Message</th>
-            <th>Completed</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forms.map((form) => (
-            <tr key={form.Id}>
-              <td key={form.Id}>{form.Id}</td>
-              <td key={form.Id}>{form.FirstName}</td>
-              <td key={form.Id}>{form.LastName}</td>
-              <td key={form.Id}>{form.Message}</td>
-              {/* // TODO - Change Completed section to display a checkmark or an X */}
-              <td key={form.Id}>
-                {form.Completed
-                  ? form.Completed.toString()
-                  : form.Completed.toString()}
-              </td>
+    forms && (
+      <div>
+        <h1>Forms Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Message</th>
+              <th>Completed</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {forms &&
+              forms.map((forms) => (
+                <tr key={forms.id}>
+                  <td>{forms.id}</td>
+                  <td>{forms.firstName}</td>
+                  <td>{forms.lastName}</td>
+                  <td>{forms.message}</td>
+                  <td>{forms.completed ? '✔️' : '❌'}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    )
   );
 };
 export default FormsTable;

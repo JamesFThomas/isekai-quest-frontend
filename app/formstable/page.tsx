30dbfcac302fe86f6b1ee907ed74d82980419078
James F. Thomas <jthomas@rsimail.com>
2025-03-13 12:48:50 -0500

    implemented logout button and userChip in FormsTableView page

--- app/formstable/page.tsx
+++ app/formstable/page.tsx
@@ -7,6 +7,11 @@ import FormsModal from '@/components/FormsModal';
 
 import Container from '@mui/material/Container';
 import CircularProgress from '@mui/material/CircularProgress';
+import { useSearchParams } from 'next/navigation';
+import { Box, Stack } from '@mui/material';
+import LogOutButton from '@/components/LogOutButton';
+import { UserData } from '../page';
+import UserChip from '@/components/UserChip';
 
 export type Form = {
   Id: number;
@@ -20,7 +25,15 @@ export const dynamic = 'force-dynamic';
 
 export default function FormsTableView() {
   const [forms, setForms] = useState<Form[] | undefined>(undefined);
+
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const searchParams = useSearchParams();
+
+  const [urlParams, setUrlParams] = useState<UserData>({
+    username: null,
+    email: null,
+  });
 
   const [formData, setFormData] = useState<Form | undefined>(undefined);
 
@@ -47,7 +60,13 @@ export default function FormsTableView() {
     if (!forms) {
       fetchReFetchData();
     }
-  }, [fetchReFetchData, forms]);
+    if (searchParams.get('username') && searchParams.get('email')) {
+      setUrlParams({
+        username: searchParams.get('username'),
+        email: searchParams.get('email'),
+      });
+    }
+  }, [fetchReFetchData, forms, searchParams]);
 
   if (forms === undefined) {
     return <CircularProgress size="3rem" />;
@@ -58,20 +77,32 @@ export default function FormsTableView() {
       sx={{
         backgroundColor: '#f5f5f5',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
       }}
       maxWidth={false}
     >
-      {forms && (
-        <AllFormsTable
-          formsData={forms}
-          refreshData={fetchReFetchData}
-          setIsModalOpen={setIsModalOpen}
-          setFormData={setFormData}
-        />
-      )}
+      <Box>
+        <Stack
+          direction="row"
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          mb={1}
+        >
+          <LogOutButton />
+          <UserChip user={urlParams} />
+        </Stack>
+        {forms && (
+          <AllFormsTable
+            formsData={forms}
+            refreshData={fetchReFetchData}
+            setIsModalOpen={setIsModalOpen}
+            setFormData={setFormData}
+          />
+        )}
+      </Box>
       {isModalOpen && (
         <FormsModal
           open={isModalOpen}

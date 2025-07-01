28d9346732c705b507c267fd00f24a1ae376e77e
JamesFThomas <james.fthomas@yahoo.com>
2025-07-01 15:20:51 -0500

    (ui) character avatar grid skeleton

--- app/createcharacter/page.tsx
+++ app/createcharacter/page.tsx
@@ -3,23 +3,64 @@
 export default function CreateCharacter() {
   return (
     <div
-      className='flex flex-col items-center p-8 min-h-screen'
+      className='flex flex-col items-center justify-center p-8 min-h-screen'
       style={{
         backgroundColor: '#d9d9d9',
       }}
     >
-      <div className='flex flex-col items-center w-fit'>
+      <div className='flex flex-col items-center'>
         <header
-          className='text-center text-5xl text-white pt-3.5 pb-3.5'
+          className='text-center text-5xl text-white p-3.5'
           style={{
             backgroundColor: '#C87D7D',
             maxWidth: '600px',
             width: '100%',
           }}
         >
-          Create Character
+          Choose Your Avatar
         </header>
         <div
+          className='mt-4'
+          style={{
+            backgroundColor: 'white',
+            maxWidth: '600px',
+            minHeight: 'fit-content',
+            width: '100%',
+          }}
+        >
+          <div className='grid grid-cols-3 gap-4 p-4'>
+            <div className='bg-gray-200 h-32 flex items-center justify-center'>
+              Avatar 1
+            </div>
+            <div className='bg-gray-200 h-32 flex items-center justify-center'>
+              Avatar 2
+            </div>
+            <div className='bg-gray-200 h-32 flex items-center justify-center'>
+              Avatar 3
+            </div>
+            <div className='bg-gray-200 h-32 flex items-center justify-center'>
+              Avatar 4
+            </div>
+            <div className='bg-gray-200 h-32 flex items-center justify-center'>
+              Avatar 5
+            </div>
+            <div className='bg-gray-200 h-32 flex items-center justify-center'>
+              Avatar 6
+            </div>
+          </div>
+        </div>
+        <div
+          className='mt-4 text-white'
+          style={{
+            backgroundColor: '#C87D7D',
+            maxWidth: '600px',
+            minHeight: '400px',
+            width: '100%',
+          }}
+        >
+          Avatar Preview + Data
+        </div>
+        <div // Button Box
           className='flex flex-col sm:flex-row justify-around w-full mt-3'
           style={{ backgroundColor: '#d9d9d9' }}
         >
@@ -32,7 +73,7 @@ export default function CreateCharacter() {
             }}
             // onClick={handleStartQuest}
           >
-            Create
+            Create Character
           </button>
         </div>
       </div>

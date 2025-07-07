f187d1aa64d5691fccfd61998025f9925e0e9855
JamesFThomas <james.fthomas@yahoo.com>
2025-07-07 10:21:35 -0500

    (data) added type attribute to avatar Image objects

--- data/avatarOptions.ts
+++ data/avatarOptions.ts
@@ -3,31 +3,37 @@ const avatarImages = [
     id: 1,
     src: '/barbarian_avatar.png',
     alt: 'Barbarian Avatar',
+    type: 'Barbarian',
   },
   {
     id: 2,
     src: '/dwarf_avatar.png',
     alt: 'Dwarf Avatar',
+    type: 'Dwarf',
   },
   {
     id: 3,
     src: '/paladin_avatar.png',
     alt: 'Paladin Avatar',
+    type: 'Paladin',
   },
   {
     id: 4,
     src: '/halfling_avatar.png',
     alt: 'Halfling Avatar',
+    type: 'Halfling',
   },
   {
     id: 5,
     src: '/necromancer_avatar.png',
     alt: 'Necromancer Avatar',
+    type: 'Necromancer',
   },
   {
     id: 6,
     src: '/snowElf_avatar.png',
     alt: 'Snow Elf Avatar',
+    type: 'Snow Elf',
   },
 ];
 

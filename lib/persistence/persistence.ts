345ae72291217dad5634d6ef72be22ffafaa045f
James F. Thomas <57680469+JamesFThomas@users.noreply.github.com>
2026-04-20 12:22:29 -0500

    added logs to check env values in deployed version

--- lib/persistence/persistence.ts
+++ lib/persistence/persistence.ts
@@ -40,6 +40,9 @@ const persistenceMode: PERSISTENCE_MODE =
 const implementation: Persistence =
   persistenceMode === 'local' ? localPersistence : remotePersistence;
 
+console.log('ENV VALUE:', process.env.NEXT_PUBLIC_PERSISTENCE_MODE);
+console.log(`Persistence mode set to: ${persistenceMode}`);
+
 /**
  * Public persistence interface used throughout the application.
  *
